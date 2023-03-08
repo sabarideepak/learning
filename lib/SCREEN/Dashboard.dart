edca9b521cb3e394f9ff469952494f36abb080c4
sabarideepak <deepaksabari2911@gmail.com>
2023-03-08 20:21:14 +0530

    section 2 image adding full screen

--- lib/SCREEN/Dashboard.dart
+++ lib/SCREEN/Dashboard.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:flutter/material.dart';
 
@@ -16,14 +17,32 @@ class Dashboard extends StatelessWidget {
         ),
       ),
       body: Center(
+        child: IconButton(
+          icon: const Icon(FontAwesomeIcons.apple, shadows: [
+            Shadow(
+              color: Colors.white,
+              blurRadius: 0,
+              offset: Offset(0, 5.0),
+            )
+          ]),
+          onPressed: () {},
+          color: Colors.blue,
+          splashColor: Colors.white,
+          highlightColor: Colors.deepOrange,
+          iconSize: 300,
+        ),
+      ),
+    );
+
+    /* Center(   ------------>image additing
         child: Image.asset(
           'images/3d-illustration-workspace.jpg.pagespeed.ce.Fx4pca0q_y.jpg',
           width: 900,
           height: 1200,
           fit: BoxFit.cover,
         ),
-      ),
-    );
+
+      ),*/
   }
 }
 

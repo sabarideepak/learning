6d45e9d7e30816d9b0a3ef44a22b080a9a8e2801
Sabarideepak <deepaksabari2911@gmail.com>
2023-03-21 23:51:58 +0530

    incre decre page

--- lib/SCREEN/decrement.dart
+++ lib/SCREEN/decrement.dart
@@ -12,43 +12,70 @@ class _decrementState extends State<decrement> {
 
   void increment() {
     setState(() {
-      i++;
+      if (i < 0) {
+        i = 0;
+      } else {
+        i++;
+      }
     });
   }
 
   void decre() {
     setState(() {
-      i--;
+      if (i <= 0) {
+        i = 0;
+      } else {
+        i--;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    var _mediaQuery = MediaQuery.of(context);
     return Scaffold(
-      body: Center(
-        child: ListView(
-          padding: const EdgeInsets.only(top: 750),
-          children: [
-            ListTile(
-              leading: FloatingActionButton(
-                  onPressed: increment,
-                  tooltip: 'increment',
-                  child: const Icon(Icons.add)),
-              title: Padding(
-                padding: const EdgeInsets.only(left: 150.0),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.topCenter,
+                width: _mediaQuery.size.width * 0.5,
+                height: _mediaQuery.size.height * 0.5,
                 child: Text(
-                  'COUNT $i',
+                  "Count $i",
                   style: const TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 30,
-                      color: Colors.red),
+                      color: Colors.blue),
                 ),
               ),
-              trailing: FloatingActionButton(
-                  onPressed: decre, child: const Icon(Icons.remove)),
-            ),
-          ],
-        ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                width: _mediaQuery.size.width * 0.1,
+                height: _mediaQuery.size.height * 0.1,
+                child: FloatingActionButton(
+                    onPressed: increment,
+                    tooltip: 'increment',
+                    child: const Icon(Icons.add)),
+              ),
+              Container(
+                width: _mediaQuery.size.width * 0.1,
+                height: _mediaQuery.size.height * 0.1,
+                child: FloatingActionButton(
+                    onPressed: decre,
+                    tooltip: 'decrement',
+                    child: const Icon(Icons.remove)),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }

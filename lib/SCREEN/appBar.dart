b055949e5ea2bb2400d00a170b3acf9248f07877
Sabarideepak <deepaksabari2911@gmail.com>
2023-04-12 18:44:10 +0530

    Youtube

--- lib/SCREEN/appBar.dart
+++ lib/SCREEN/appBar.dart
@@ -298,6 +298,16 @@ class _AppbarState extends State<Appbar> {
                       },
                       child: const Text(getString.BottomImageUpload),
                     ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(
+                            context, Routerclass.UtubeHomepagebtn);
+                      },
+                      child: const Text(getString.Utubehomepagebtn),
+                    ),
                   )
                 ],
               ),
---
+++ lib/SCREEN/youtube/add.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Add3 extends StatefulWidget {
+  const Add3({Key? key}) : super(key: key);
+
+  @override
+  State<Add3> createState() => _Add3State();
+}
+
+class _Add3State extends State<Add3> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

2ce90985ecc7ed9a7be2c10925970a41dbd060e7
Sabarideepak <deepaksabari2911@gmail.com>
2023-04-23 13:30:03 +0530

    Uidesign

---
+++ lib/SCREEN/AMAZON/UI_Design/Archived.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Archived extends StatefulWidget {
+  const Archived({Key? key}) : super(key: key);
+
+  @override
+  State<Archived> createState() => _ArchivedState();
+}
+
+class _ArchivedState extends State<Archived> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

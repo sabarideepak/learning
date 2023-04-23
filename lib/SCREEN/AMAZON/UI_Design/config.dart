7c1e0b97319ec10e7233a20a4d7d337e5830f02b
Sabarideepak <deepaksabari2911@gmail.com>
2023-04-23 13:29:40 +0530

    Uidesign

---
+++ lib/SCREEN/AMAZON/UI_Design/config.dart
@@ -0,0 +1,14 @@
+class textString {
+  static const BottomIcon1 = 'Next';
+  static const BottomIcon2 = 'Waiting';
+  static const BottomIcon3 = 'Schedulded';
+  static const BottomIcon4 = 'Someday';
+  static const Bottommainlist1 = 'Design Project';
+  static const BottomChildlist = 'Child Project';
+  static const Bottommainlist2 = 'Development Project';
+  static const Bottommainlist3 = 'Wireframe For Vocabforever';
+  static const Bottommainlist4 = 'Demo Name 01';
+  static const Bottommainlist5 = 'Demo Name 02';
+  static const Bottommainlist6 = 'Demo Name 03';
+  static const SubTextpara = 'Lorem ipsum dolor sit amet,consect....';
+}
